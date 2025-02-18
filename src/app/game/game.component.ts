98e8f305aac155be1ac51c2c5a68825639ef0fb6
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-02-18 11:46:24 +0100

    show random card

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,13 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Game } from 'src/modules/game';
 
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
-  styleUrls: ['./game.component.scss']
+  styleUrls: ['./game.component.scss'],
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
   pickCardAnimation = false;
-  takeCard(){
-    this.pickCardAnimation = true;
+  currentCard: string = '';
+  game!: Game;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      const card = this.game.stack.pop();
+      if (card !== undefined) {
+        this.currentCard = card;
+        console.log(this.currentCard);
+        this.pickCardAnimation = true;
+
+        setTimeout(() => {
+          this.pickCardAnimation = false;
+        }, 1500);
+      } else {
+        console.log('No more cards to take.');
+      }
+    }
   }
 }
