cea8e98feb76b37288c1b0ffdc6b23c5f82ea46f
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2025-02-20 10:51:07 +0100

    dispaly active player

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Game } from 'src/modules/game';
 import { MatDialog } from '@angular/material/dialog';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
+import { throttle } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -43,6 +44,13 @@ export class GameComponent implements OnInit {
           this.game.playedCards.push(this.currentCard);
           this.pickCardAnimation = false;
         }, 1000);
+        if (this.game.currentPlayer < this.game.players.length - 1) {
+                  this.game.currentPlayer ++;
+        } else {
+          this.game.currentPlayer = 0; 
+        }
+        console.log(this.game.players[this.game.currentPlayer]);
+        
       } else {
         console.log('No more cards to take.');
       }
